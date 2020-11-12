f8cc629d32c01ceb819d463c74e88e90e9ad6ced
Azari80 <63757784+Azari80@users.noreply.github.com>
2020-11-11 23:07:24 -0500

    ok

---
+++ Komlan-Gblokpor-Koffi_Examen2 2/js/validationDeFormulaire.js
@@ -0,0 +1,29 @@
+<!--
+validationDeFormulaire.js
+Komlan Albert Gblokpor-Koffi
+Dernière modification : 25-10-2020
+-->
+function validationFormulaire() {
+    let codeRegional = document.getElementById("phone");
+
+    let codeRegionalPermise = false;
+
+    switch(codeRegional.value) {
+        case "819":
+            codeRegionalPermise = true;
+            break;
+        case "418":
+            codeRegionalPermise = true;
+            break;
+        case "514":
+            codeRegionalPermise = true;
+            break;
+    }
+
+    if(codeRegionalPermise === false){
+        alert(" SVP Vous n'avaez pas entré le bon CODE REGIONAL !!!! Veuillez choisir un code regional entre  ceux-ci: 819; 418; 514 MERCI !!!! ");
+    }
+    return codeRegionalPermise;
+}
+
+
